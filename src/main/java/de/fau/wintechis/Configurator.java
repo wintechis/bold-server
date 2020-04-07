374a2bb51cb1ec6391ff496f22f45f40228735c8
Victor Charpenay <victor.charpenay@fau.de>
2020-04-07 18:18:50 +0200

    renamed project: bold-benchmark

--- src/main/java/de/fau/wintechis/Configurator.java
+++ src/main/java/de/fau/wintechis/Configurator.java
@@ -9,17 +9,17 @@ import java.util.Properties;
 
 public class Configurator {
 
-    private final static String SERVER_HTTP_PORT_KEY = "boldng.server.httpPort";
+    private final static String SERVER_HTTP_PORT_KEY = "bold.server.httpPort";
 
     private final static String SERVER_HTTP_PORT_DEFAULT = "8080";
 
-    private final static String INIT_DATASET_KEY = "boldng.init.dataset";
+    private final static String INIT_DATASET_KEY = "bold.init.dataset";
 
-    private final static String INIT_UPDATE_KEY = "boldng.init.update";
+    private final static String INIT_UPDATE_KEY = "bold.init.update";
 
-    private final static String RUNTIME_UPDATE_KEY = "boldng.runtime.update";
+    private final static String RUNTIME_UPDATE_KEY = "bold.runtime.update";
 
-    private final static String RUNTIME_QUERY_KEY = "boldng.runtime.query";
+    private final static String RUNTIME_QUERY_KEY = "bold.runtime.query";
 
     public static void main(String[] args) throws IOException {
         // TODO more advanced CLI
