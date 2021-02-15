6e1f426a79d714c2b974c395c7b925133e4a4d40
Victor Charpenay <victor.charpenay@fau.de>
2021-02-15 23:13:19 +0100

    added static file handler for docs

--- src/main/java/de/fau/wintechis/sim/SimulationHandler.java
+++ src/main/java/de/fau/wintechis/sim/SimulationHandler.java
@@ -4,8 +4,8 @@ import de.fau.wintechis.gsp.GraphStoreHandler;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
-import org.eclipse.rdf4j.repository.config.RepositoryRegistry;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.rdf4j.repository.sail.SailRepository;
 import org.eclipse.rdf4j.repository.sail.SailRepositoryConnection;
 import org.eclipse.rdf4j.sail.NotifyingSailConnection;
@@ -22,11 +22,15 @@ public class SimulationHandler extends AbstractHandler {
 
     public static final String SIMULATION_RESOURCE_TARGET = "/sim";
 
+    public static final String PUBLIC_RESOURCE_FOLDER = "doc";
+
     private final Logger log = LoggerFactory.getLogger(SimulationHandler.class);
 
     private final Server server;
 
-    private final GraphStoreHandler handler;
+    private final ResourceHandler staticHandler;
+
+    private final GraphStoreHandler gsHandler;
 
     private final SimulationEngine engine;
 
@@ -34,6 +38,13 @@ public class SimulationHandler extends AbstractHandler {
         server = new Server(port);
         server.setHandler(this);
         server.start();
+
+        // TODO isn't there a handler collection to do the job instead?
+        staticHandler = new ResourceHandler();
+        staticHandler.setBaseResource(Resource.newResource(PUBLIC_RESOURCE_FOLDER));
+        staticHandler.setDirectoriesListed(false);
+        staticHandler.setWelcomeFiles(new String[] { "index.html" });
+        staticHandler.doStart();
 
         MemoryStore store = new MemoryStore();
         SailRepository repo = new SailRepository(store);
@@ -51,8 +62,8 @@ public class SimulationHandler extends AbstractHandler {
         // TODO have a handler thread pool (see org.eclipse.jetty.util.thread.QueuedThreadPool)
         // TODO manage RepositoryConnections for all individual threads
         // note: server's base URI is set only after server starts
-        handler = new GraphStoreHandler(server.getURI(), handlerConnection);
-        handler.addGraphStoreListener(interactions);
+        gsHandler = new GraphStoreHandler(server.getURI(), handlerConnection);
+        gsHandler.addGraphStoreListener(interactions);
 
         log.info("Server started on port {}. Waiting for command on resource {}...", port, SIMULATION_RESOURCE_TARGET);
     }
@@ -67,12 +78,15 @@ public class SimulationHandler extends AbstractHandler {
 
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        staticHandler.handle(target, baseRequest, request, response);
+        if (baseRequest.isHandled()) return; // static resource was found
+
         switch (engine.getCurrentState()) {
             case EMPTY_STORE:
                 // only recognizes PUT /sim
                 // TODO check validity of RDF payload with shape
                 if (request.getMethod().equals("PUT") && target.equals(SIMULATION_RESOURCE_TARGET)) {
-                    handler.handle(target, baseRequest, request, response);
+                    gsHandler.handle(target, baseRequest, request, response);
                     engine.callTransition();
                 } else {
                     response.sendError(HttpServletResponse.SC_NOT_FOUND);
@@ -81,7 +95,7 @@ public class SimulationHandler extends AbstractHandler {
                 break;
 
             case RUNNING:
-                handler.handle(target, baseRequest, request, response);
+                gsHandler.handle(target, baseRequest, request, response);
                 break;
 
             default:
