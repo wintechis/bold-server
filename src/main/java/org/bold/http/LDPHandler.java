a835f1e2826a7b72ff26ed65f0047f2c7104a3ff
Daniel Schraudner <daniel.schraudner@fau.de>
2021-05-28 12:07:22 +0200

    Basic Tests for LDPHandler

--- src/main/java/org/bold/http/LDPHandler.java
+++ src/main/java/org/bold/http/LDPHandler.java
@@ -1,20 +1,20 @@
 package org.bold.http;
 
+import org.bold.http.GraphListener;
+import org.bold.io.RDFValueFormats;
 import org.bold.sim.Vocabulary;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFHandler;
-import org.eclipse.rdf4j.rio.RDFParseException;
-import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -22,7 +22,6 @@ import java.util.Set;
  * RESTful access to named graphs in an RDF dataset.
  *
  * TODO ASK queries (~ RDF shapes) to control resource handler I/O
- * TODO request logger (for evaluation)
  */
 public class LDPHandler extends AbstractHandler implements GraphHandler {
 
@@ -53,9 +52,25 @@ public class LDPHandler extends AbstractHandler implements GraphHandler {
 
         boolean created = !exists(graphName);
 
-        // TODO send 406 Not Acceptable and 415 Unsupported Media Type if header present?
-        RDFFormat accept = Rio.getParserFormatForMIMEType(request.getHeader("Accept")).orElse(DEFAULT_RDF_FORMAT);
-        RDFFormat contentType = Rio.getParserFormatForMIMEType(request.getHeader("Content-Type")).orElse(DEFAULT_RDF_FORMAT);
+        // TODO use a ServletFilter instead, for processing Accept/Content-Type
+
+        String acceptString = request.getHeader("Accept");
+        RDFFormat accept = getFormatForMediaType(acceptString);
+
+        if (accept == null) {
+            response.sendError(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            baseRequest.setHandled(true);
+            return;
+        }
+
+        String contentTypeString = request.getHeader("Content-Type");
+        RDFFormat contentType = getFormatForMediaType(contentTypeString);
+
+        if (contentType == null) {
+            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+            baseRequest.setHandled(true);
+            return;
+        }
 
         long before, after;
 
@@ -65,8 +80,9 @@ public class LDPHandler extends AbstractHandler implements GraphHandler {
                     if (!created) {
                         response.setHeader("Content-Type", accept.getDefaultMIMEType());
 
+                        RDFWriter writer = Rio.createWriter(accept, response.getOutputStream());
+
                         before = System.currentTimeMillis();
-                        RDFHandler writer = Rio.createWriter(accept, response.getOutputStream());
                         connection.export(writer, graphName);
                         after = System.currentTimeMillis();
 
@@ -137,6 +153,17 @@ public class LDPHandler extends AbstractHandler implements GraphHandler {
 
     private boolean exists(IRI graphName) {
         return connection.hasStatement(null, null, null, false, graphName);
+    }
+
+    private RDFFormat getFormatForMediaType(String mediaType) {
+        if (mediaType == null || mediaType.equals("*/*")) return DEFAULT_RDF_FORMAT;
+
+        // TODO parse conneg header if more complex value
+
+        Optional<RDFFormat> opt = Rio.getParserFormatForMIMEType(mediaType);
+        if (opt.isPresent()) return opt.get();
+
+        return RDFValueFormats.getFormatForMediaType(mediaType);
     }
 
 }
---
+++ src/test/java/org/bold/http/LDPHandlerTest.java
@@ -0,0 +1,180 @@
+package org.bold.http;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.bold.io.FileUtils;
+import org.bold.sim.SimulationEngine;
+import org.bold.sim.SimulationHandler;
+import org.eclipse.rdf4j.common.io.IOUtil;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.StringReader;
+import java.nio.Buffer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LDPHandlerTest {
+
+    private static final Integer TEST_PORT = 8080;
+
+    private static final String EXPECTED_JSON = "{\"val\":10}";
+
+    private SimulationHandler handler = null;
+    private SimulationEngine ngin = null;
+
+    @Before
+    public void createEngine() throws Exception {
+        handler = new SimulationHandler(TEST_PORT, "ldp");
+        ngin = handler.getSimulationEngine();
+    }
+
+    @After
+    public void closeEngine() {
+        try {
+            handler.terminate();
+            clearResults();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // and ignore...
+        }
+    }
+
+    @Test
+    public void testAccept() throws Exception {
+        ngin.registrationDone();
+
+        Map<String, String> h = new HashMap<>();
+        h.put("Accept", "video/mp4");
+
+        startSimulation("sim.ttl");
+        int status = sendDummyRequest(h);
+
+        assertEquals(406, status);
+    }
+
+    @Test
+    public void testAcceptWildcard() throws Exception {
+        ngin.registrationDone();
+
+        Map<String, String> h = new HashMap<>();
+        h.put("Accept", "*/*");
+
+        startSimulation("sim.ttl");
+        int status = sendDummyRequest(h);
+
+        assertEquals(404, status);
+    }
+
+    @Test
+    public void testContentType() throws Exception {
+        ngin.registrationDone();
+
+        Map<String, String> h = new HashMap<>();
+        h.put("Content-Type", "video/mp4");
+
+        startSimulation("sim.ttl");
+        int status = sendDummyRequest(h);
+
+        assertEquals(415, status);
+    }
+
+    @Test
+    public void testNotFound() throws Exception {
+        ngin.registrationDone();
+
+        startSimulation("sim.ttl");
+        int status = sendDummyRequest();
+
+        assertEquals(404, status);
+    }
+
+    @Test
+    public void testJSONValue() throws Exception {
+        ngin.registerDataset("json-value.trig").registrationDone();
+
+        Map<String, String> h = new HashMap<>();
+        h.put("Accept", "application/json");
+
+        startSimulation("sim.ttl");
+        String rep = getRepresentation("val", h);
+
+        assertEquals(EXPECTED_JSON, rep);
+    }
+
+    @Test
+    public void testJSONNoValue() throws Exception {
+        ngin.registerDataset("json-no-value.trig").registrationDone();
+
+        Map<String, String> h = new HashMap<>();
+        h.put("Accept", "application/json");
+
+        startSimulation("sim.ttl");
+        String rep = getRepresentation("val", h);
+
+        assertEquals(0, rep.length());
+    }
+
+    private static String getRepresentation(String path, Map<String, String> headers) throws Exception {
+        CloseableHttpClient client = HttpClients.createMinimal();
+        HttpGet req = new HttpGet("http://localhost:" + TEST_PORT + "/" + path);
+
+        for (Map.Entry<String, String> kv : headers.entrySet()) {
+            req.addHeader(kv.getKey(), kv.getValue());
+        }
+
+        CloseableHttpResponse resp = client.execute(req);
+        return IOUtil.readString(resp.getEntity().getContent());
+    }
+
+    // TODO code below is duplicated from SimulationEngineTest, use helper class instead
+
+    private static int sendDummyRequest() throws Exception {
+        return sendDummyRequest(new HashMap<>());
+    }
+
+    private static int sendDummyRequest(Map<String, String> headers) throws Exception {
+        return sendDummyRequest("not-found", headers);
+    }
+
+    private static int sendDummyRequest(String path, Map<String, String> headers) throws Exception {
+        CloseableHttpClient client = HttpClients.createMinimal();
+        HttpGet req = new HttpGet("http://localhost:" + TEST_PORT + "/" + path);
+
+        for (Map.Entry<String, String> kv : headers.entrySet()) {
+            req.addHeader(kv.getKey(), kv.getValue());
+        }
+
+        CloseableHttpResponse resp = client.execute(req);
+        return resp.getStatusLine().getStatusCode();
+    }
+
+    private static int startSimulation(String filename) throws Exception {
+        CloseableHttpClient client = HttpClients.createMinimal();
+        HttpPut req = new HttpPut("http://localhost:" + TEST_PORT + "/sim");
+        req.addHeader("Content-Type", "text/turtle");
+        req.setEntity(new InputStreamEntity(FileUtils.getFileOrResource(filename)));
+        CloseableHttpResponse resp = client.execute(req);
+        return resp.getStatusLine().getStatusCode();
+    }
+
+    private static void clearResults() {
+        File[] files = new File(".").listFiles((File f) -> f.getName().endsWith(".tsv"));
+
+        for (File f : files) {
+            f.delete();
+        }
+    }
+
+}
